f52923f651228f31d9303cb7d95d7babff73cb9f
Codder12346768 <dharmip060@gmail.com>
2023-12-11 01:47:12 +0530

    Research paper info changed

--- src/Components/CallForPaper.js
+++ src/Components/CallForPaper.js
@@ -9,7 +9,11 @@ export default function CallForPaper() {
       <Navbar />
       <h1 className="callforpaper-heading">Call for Papers</h1>
       <div className="text-container">
-        <img src="ijhe.jpeg" alt="ijhe" />
+        <p><b>Abstract submission guidelines:</b></p>
+        <p>The abstract of the paper should concisely articulate the contextual framework and purpose of the study, provide a succinct overview of the methodology employed, present preliminary findings, and emphasize noteworthy aspects of the research. The abstract should be constrained to a maximum of 500 words. </p>
+        <p><b>Paper Submission guidelines:</b></p>
+        <p>The abstract and full article may be submitted to the email address <a href="mailto:itcoges@pdpu.ac.in">itcoges@pdpu.ac.in. </a>The conference website <a href="www.icoges2024.com">www.icoges2024.com</a>will provide the author with submission details and requirements for abstract and complete paper preparation. The papers that have been submitted will undergo evaluation by the technical committee. All papers that are deemed acceptable will be incorporated into the electronic proceedings of the conference.  </p>
+        {/* <img src="ijhe.jpeg" alt="ijhe" />
         <p className="callforpaper-text">
           The selected papers will be published in <br />
           <a href="https://www.journals.elsevier.com/international-journal-of-hydrogen-energy/call-for-papers/1st-international-conference-on-green-hydrogen-for-global-de-carbonization">
@@ -18,20 +22,33 @@ export default function CallForPaper() {
           </a>{" "}
           <br />
           (Impact Factor: 7.1)
-        </p>
+        </p> */}
       </div>
-      <p className="submission-text">
-        Last date for submission of abstract is <s>10<sup>th</sup> February 2023</s> <b>28<sup>th</sup> Feburary 2023.</b>
+      <p className="">
+       <h1 className="callforpaper-heading">Important Dates</h1> 
+       <div className="imp-dates">
+       <p>Call for Abstracts deadline: 10 Jan 2024</p>
+      <p>Acceptance notification: 15 Jan 2024</p>
+      <p>Early registration deadline: 20 Jan 2024</p>
+      <p>Regular registration deadline: 30 Jan 2024</p>
+      <p>Conference dates:  16 and 17 Feb 2024</p>
+       </div>
+       
+
+
+
+
+
         
       </p>
       <div className="button-container">
-        <button className="callforpaper-button">
+        {/* <button className="callforpaper-button">
           <a href="https://hpurl.ga/abstract">Download the abstract template</a>
-        </button>
+        </button> */}
       </div>
 
       <p className="mail-text">
-        Please submit your abstracts to <a href="https://forms.gle/a7g4aAtJZuepZmNdA"> Google Form</a>
+        {/* Please submit your abstracts to <a href="https://forms.gle/a7g4aAtJZuepZmNdA"> Google Form</a> */}
       </p>
       <Footer />
     </div>
